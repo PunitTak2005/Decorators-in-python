ef4d58cc26022fe2bb24f831a5cd45806faa63c7
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-09 17:19:17 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,12 @@
-import time
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds.")
-        return result
-    return wrapper
+
+
+
+
+
+
+def greet(name, greeting="Hello'0:"):
+    print(f"{greeting},{name}")
+
+
+greet("chai", greeting="hanji")
